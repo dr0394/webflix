6fece8fab34a3b6ddfff5378cb24ee8afbd84205
webflixinfo <kontakt@webflix.info>
2026-03-18 16:37:10 +0100

    Updated AboutPage.tsx

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -162,7 +162,7 @@ const AboutPage = () => {
                   <img
                     src={member.image}
                     alt={member.name}
-                    className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
+                    className="w-full h-full object-cover object-top group-hover:scale-105 transition-transform duration-500"
                   />
                   <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
                 </div>
