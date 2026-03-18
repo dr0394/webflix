8b91df20045920b91e52895e783457396ecb71a6
webflixinfo <kontakt@webflix.info>
2026-03-18 13:53:41 +0100

    Add new logo to nav and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
       <div className="container mx-auto">
         <div className="mb-8 sm:mb-10">
           <img
-            src="/WEbflix_mdd_ohne_(2).png"
+            src="/Webflixlogo_nurlogo.png"
             alt="Webflix"
             className="h-16 sm:h-20 md:h-24 lg:h-28 w-auto"
           />
